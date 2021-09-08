807eaa83810fdbf68cc357ba6c566198d0acb0ba
Phil Proctor <philliptproctor@gmail.com>
2021-09-07 23:39:22 -0400

    Reorg charts

--- debug.sh
+++ debug.sh
@@ -3,7 +3,7 @@
 CMD=$1
 
 if [[ $CMD == 'install' ]]; then
-    helm install -f debug-vals.yml -n csi zed-csi-debug .
+    helm install -f debug-vals.yml -n csi zed-csi-debug charts/zed-csi
 elif [[ $CMD == 'uninstall' ]]; then
     helm uninstall -n csi zed-csi-debug
 fi
--- verify.sh
+++ verify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-helm template --debug . >debug.yml
+helm template --debug charts/zed-csi >debug.yml
 kubeval debug.yml
